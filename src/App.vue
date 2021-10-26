6cab32546c617d2835025bd75932716374165ae2
camelot1399 <camelot1399@mail.ru>
2021-10-26 19:44:03 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="showTable">Сгенерировать</button>
+
+    <TableCompoennt 
+      v-if="shownTable"
+      :data="inTableData"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableCompoennt from "@/components/Table";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      shownTable: false,
+      inTableData: [],
+    }
+  },
   components: {
-    HelloWorld
+    TableCompoennt
+  },
+  methods: {
+    showTable() {
+      this.inTableData = [];  // обнуляем массив на случай повторого нажатия на кнопку: сгенерировать
+      this.shownTable = !this.shownTable;
+      this.generateTable();
+    },
+    generateTable() {
+      const countRows = 50;   // кол-во сток
+      const minPrice = 0.01;  // минимальная цена
+      const maxPrice = 1000;  // максимальная цена
+      const minCount = 1;     // минимальное кол-во
+      const maxCount = 100;   // максимальное кол-во
+
+      console.log(minPrice.toFixed(2));
+      console.log(maxPrice.toFixed(2));
+
+      console.log(maxPrice - minPrice);
+      
+
+      for (let i = 0; i < countRows; i++) {
+        let item = {};
+        item.id = i;
+        item.name = this.generateLetters();
+        item.price = this.getRandomNumber(minPrice, maxPrice).toFixed(2);
+        item.count = this.getRandomNumber(minCount, maxCount).toFixed(0);
+
+        this.inTableData.push(item);
+      }
+    },
+    getRandomNumber(min, max) {
+      return Math.random() * (max - min) + min;
+    },
+    generateLetters() {
+      let result = '';
+      const countLetters = 5;  // кол-во букв
+      const russianLetters = 'яюэьыъщшчцхфутсрпонмлкйизжёедгвба';
+      const max_position = russianLetters.length - 1;
+      
+      for(let i = 0; i < countLetters; ++i ) {
+          let position = Math.floor(Math.random() * max_position);
+          result = result + russianLetters.substring(position, position + 1);
+      }
+
+      return result;
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Table/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+		<table>
+			<thead>
+				<tr>
+					<td>Наименование</td>
+					<td>Цена</td>
+					<td>Количество</td>
+					<td>Стоимость</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item of data" :key="item.id">
+					<td>{{ item.name }}</td>
+					<td>{{ item.price }}</td>
+					<td>{{ item.count }}</td>
+					<td>{{ (item.price * item.count).toFixed(2) }}</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div>Итоговая стоимость: <span>{{ totalPrice }}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'tableComponent',
+	props: {
+		data: {
+			type: Array
+		}
+	},
+	computed: {
+		totalPrice() {
+			return this.data.reduce((acc, item) => {
+				return acc + (item.price * item.count);
+			}, 0).toFixed(2)
+		}
+	},
+	mounted() {
+		console.log(this.data);
+	}
+}
+</script>
+
+<style>
+table {
+    border: 1px solid #ccc;
+}
+td {
+    color: black;
+    border-right: 1px dashed #ccc;
+    padding: 10px 20px;
+}
+tbody > tr {
+    border-bottom: 1px solid #ccc;
+}
+</style>
